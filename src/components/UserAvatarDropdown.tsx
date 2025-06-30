e8172b959a21ad8c6dfcfe8738749306d046a071
Mohamed-Ali-Hammami <99651583+Mohamed-Ali-Hammami@users.noreply.github.com>
2025-06-30 18:49:26 +0100

    commit

--- src/components/UserAvatarDropdown.tsx
+++ src/components/UserAvatarDropdown.tsx
@@ -60,7 +60,7 @@ const UserAvatarDropdown: React.FC = () => {
                 }}
                 className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
               >
-                S'inscrire
+                S&apos;inscrire
               </button>
               <button
                 onClick={() => {
